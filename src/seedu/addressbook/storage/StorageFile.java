eabd4c4e2965a76ed46be608edd930c24b00462b
Bartek <bartekwozniak93@gmail.com>
2016-08-30 18:20:33 +0800

    Handle File not found while running program.

--- src/seedu/addressbook/storage/StorageFile.java
+++ src/seedu/addressbook/storage/StorageFile.java
@@ -88,6 +88,11 @@ public class StorageFile {
         /* Note: Note the 'try with resource' statement below.
          * More info: https://docs.oracle.com/javase/tutorial/essential/exceptions/tryResourceClose.html
          */
+    	if(!path.toFile().exists()){
+    		//throw new StorageOperationException("File does not exist!");
+    		System.out.println("File does not exist! It was created for you.");
+    	}
+    	
         try (final Writer fileWriter =
                      new BufferedWriter(new FileWriter(path.toFile()))) {
 
